823f556922b3ae9a6ab9e0dd78c6fbae07733ca6
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-01-25 13:29:22 +0100

    Added module with provide settings manager

---
+++ app/src/main/java/com/mateusz/grabarski/daggersimpleinjection/base/Constants.java
@@ -0,0 +1,10 @@
+package com.mateusz.grabarski.daggersimpleinjection.base;
+
+/**
+ * Created by Mateusz Grabarski on 25.01.2018.
+ */
+
+public class Constants {
+
+    public static final String SHARE_PREFS_FILE = "prefs";
+}
---
+++ app/src/main/java/com/mateusz/grabarski/daggersimpleinjection/base/di/AppModule.java
@@ -0,0 +1,34 @@
+package com.mateusz.grabarski.daggersimpleinjection.base.di;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.mateusz.grabarski.daggersimpleinjection.base.App;
+import com.mateusz.grabarski.daggersimpleinjection.base.Constants;
+import com.mateusz.grabarski.daggersimpleinjection.settings.SettingsManager;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Mateusz Grabarski on 25.01.2018.
+ */
+@Module
+public class AppModule {
+
+    private App mApp;
+
+    public AppModule(App mApp) {
+        this.mApp = mApp;
+    }
+
+    @Provides
+    SharedPreferences provideSharedPreferences() {
+        return mApp.getSharedPreferences(Constants.SHARE_PREFS_FILE, Context.MODE_PRIVATE);
+    }
+
+    @Provides
+    SettingsManager provideSettingsManager(SharedPreferences sharedPreferences) {
+        return new SettingsManager(sharedPreferences);
+    }
+}
